0cd75cba9475e82300cdb9d3efea9d2798656fec
TuanThanh100406 <thanhlehok100406@gmail.com>
2025-08-26 20:46:28 +0700

    Update models.py

--- src/backend/models.py
+++ src/backend/models.py
@@ -1,24 +1,18 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Text
 from sqlalchemy.orm import relationship
 from .database import Base
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    password = Column(String)
-    fullname = Column(String, default="")
-    history = relationship("History", back_populates="owner")
+    username = Column(String(50), unique=True, index=True)
+    password = Column(String(255))
+    preferences = Column(Text, nullable=True)  
+    budget = Column(Float, nullable=True)
 
 class Recipe(Base):
     __tablename__ = "recipes"
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    detail = Column(String)
-
-class History(Base):
-    __tablename__ = "history"
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    recipe_id = Column(Integer, ForeignKey("recipes.id"))
-    owner = relationship("User", back_populates="history")
+    name = Column(String(100), unique=True)
+    description = Column(Text)
+    nutrition = Column(Text)
